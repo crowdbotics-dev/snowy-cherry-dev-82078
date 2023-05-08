94bbd9140c745700fd88e90b34b1798ef85bbbe7
Crowdbotics <engineering@crowdbotics.com>
2023-05-08 11:43:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snowy-cherry-dev-82078",
+  url: "https://snowy-cherry-dev-82078.botics.co",
+  api: "https://snowy-cherry-dev-82078.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
